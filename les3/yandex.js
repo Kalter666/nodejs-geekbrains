55291b19d2576b452bc54181682947bf6e67f7b5
Kalter <kalterxad34@gmail.com>
2016-08-10 03:04:56 +0300

    orm

--- les3/yandex.js
+++ les3/yandex.js
@@ -1,5 +1,39 @@
 /**
  * Created by kalter on 8/10/2016.
  */
-const yandex = require('./key');
-var key = yandex.key;
+const yourkey = require('./key');
+var key = yourkey.key;
+var url = 'https://translate.yourkey.net/api/v1.5/tr/translate?';
+var request = require('request');
+const readline = require('readline');
+const cheerio = require('cheerio');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var translate = function (text) {
+    var str = url + 'key=' + key + '&text=' + text + '&lang=ru';
+    request(str, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('Translation: ' + getTranslation(body));
+            console.log(body.toString());
+        }
+    });
+};
+
+var getTranslation = function (xml) {
+    var $ = cheerio.load(xml);
+    return $('text').text();
+};
+
+
+rl.on('line', (answer) => {
+    if (answer != '') {
+        console.log('Your text: ' + answer);
+        translate(answer);
+    } else {
+        console.log('Write smth to translate');
+    }
+});
