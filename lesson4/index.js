33393c1b5265e8d7eb4af9e962bd662b04b6c8b3
Kalter <kalterxad34@gmail.com>
2016-08-12 22:19:44 +0300

    lvl1 almost done

--- lesson4/index.js
+++ lesson4/index.js
@@ -12,25 +12,32 @@ app.engine('hbs', template.handlebars);
 app.set('view engine', 'hbs');
 app.set('views',__dirname+'/views');
 
-const url = {
-    url: 'https://geekbrains.ru/',
-    posts: '#posts',
-    com: '#posts_comments'
+const url_ria = {
+    name: 'Ria news',
+    uri: 'http://ria.ru/',
+    id: ".b-index__main-list-place"
+};
+
+const url_ya = {
+    name: 'Yandex',
+    uri: 'https://news.yandex.ru/',
+    id: '.rubric__right'
 };
 
 app.get('/', (req, res) =>{
     res.render('index', {
-        title: 'Greetings'
+        title: 'Greetings',
+        cite: [url_ria.name, url_ya.name]
     });
 });
 
-function getPosts(idPost, callback) {
-    request(url.url, (err, res, body) => {
-        if (!err && res.statusCode == 200){
-            const $ = cheerio.load(body);
-            return callback($(idPost).children().children().map((i, el) => {
-                    return $(this).text();
-            }).get().join('\r\n'));
+function getPosts(url, cb) {
+    request(url.uri, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            cb($(url.id).children().children().map(function (i, el) {
+                return $(this).text();
+            }).get());
         }
     });
 }
@@ -38,18 +45,20 @@ function getPosts(idPost, callback) {
 app.post('/', (req, res) => {
     if (!req.body) return res.sendStatus(400);
     switch (req.body.type){
-        case 'p':
-            getPosts(url.posts, (news) => {
+        case url_ria.name:
+            getPosts(url_ria, (cb) => {
                 res.render('news', {
-                    title: 'Posts',
-                    content: news
+                    title: url_ria.name,
+                    content: cb
                 })
             });
             break;
-        case 'c':
-            res.render('news', {
-                title: 'Comments',
-                content: getPosts(url.com)
+        case url_ya.name:
+            getPosts(url_ya,(cb) => {
+                res.render('news', {
+                    title: url_ya.name,
+                    content: cb
+                })
             });
     }
 });
