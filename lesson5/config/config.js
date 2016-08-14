80e37c7802b21945419aefd70c60a3742be27814
Kalter <kalterxad34@gmail.com>
2016-08-14 21:28:56 +0300

    init 5

---
+++ lesson5/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kalter on 8/14/2016.
+ */
+const mysql = require('mysql');
+const pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '',
+  database        : 'tasks'
+});
+
+module.exports = pool;
---
+++ lesson5/models/tasks.js
@@ -0,0 +1,22 @@
+/**
+ * Created by kalter on 8/14/2016.
+ */
+var Tasks = {
+  list: function (callback) {
+// TODO
+  },
+  add: function (task, callback) {
+// TODO
+  },
+  change: function (id, text, callback) {
+    // TODO
+  },
+  complete: function (id, callback) {
+    // TODO
+  },
+  delete: function (id, callback) {
+    // TODO
+  }
+};
+
+module.exports = Tasks;
